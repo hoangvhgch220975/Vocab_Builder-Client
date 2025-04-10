6849ad09f9a115cc2d72de8492c382c6dbf460c3
hoang_vh24 <136774878+hoangvhgch220975@users.noreply.github.com>
2025-04-10 15:12:30 +0700

    fix add function

---
+++ .history/src/views/New_20250410151142.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>New Word</h1>
+    <word-form :word="word" @submit="onSubmit" />
+  </div>
+</template>
+
+<script>
+import { AddVocab } from "../helpers/Helper";
+import WordForm from "@/components/WordForm.vue"; // Adjust the path if necessary
+
+export default {
+  name: "NewWord",
+  components: {
+    WordForm,
+  },
+  data() {
+    return {
+      word: {
+        english: "",
+        german: "",
+      },
+    };
+  },
+  methods: {
+    async onSubmit(newWord) {
+      await AddVocab(newWord);
+      this.flash("Add new word succeed!");
+      this.$router.push("/words");
+    },
+  },
+};
+</script>
